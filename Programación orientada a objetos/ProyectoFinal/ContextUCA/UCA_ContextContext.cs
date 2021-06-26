d6160a6110444665092d605e4372f7457b45165f
Lou2714 <00090220@uca.edu.sv>
2021-06-26 12:53:48 -0600

    Update

--- Programación orientada a objetos/ProyectoFinal/ContextUCA/UCA_ContextContext.cs
+++ Programación orientada a objetos/ProyectoFinal/ContextUCA/UCA_ContextContext.cs
@@ -36,7 +36,7 @@ namespace ProyectoFinal.ContextUCA
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-00OSCEQ3;Database=UCA_Context;Trusted_Connection=True");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=Proyecto_Final_BD_POO_2021;Trusted_Connection=True;");
             }
         }
 

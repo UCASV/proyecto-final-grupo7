3d63570367782e540c1059cffaaa872cf50c6c9f
E342 <00161220@uca.edu.sv>
2021-06-27 14:41:46 -0600

    Adding Login

--- Programación orientada a objetos/ProyectoFinal/View/Login.Designer.cs
+++ Programación orientada a objetos/ProyectoFinal/View/Login.Designer.cs
@@ -1,7 +1,6 @@
-﻿
-namespace ProyectoPrueba
+﻿namespace ProyectoPrueba
 {
-    partial class Login
+    partial class frmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,15 +28,17 @@ namespace ProyectoPrueba
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.txtUser = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.user = new System.Windows.Forms.Label();
             this.password = new System.Windows.Forms.Label();
-            this.btnLogIn = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtWelcome = new System.Windows.Forms.Label();
+            this.txtBooth = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblBooth = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -54,23 +55,25 @@ namespace ProyectoPrueba
             this.tableLayoutPanel1.Controls.Add(this.txtPassword, 2, 5);
             this.tableLayoutPanel1.Controls.Add(this.user, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.password, 1, 5);
-            this.tableLayoutPanel1.Controls.Add(this.btnLogIn, 2, 7);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 2, 2);
             this.tableLayoutPanel1.Controls.Add(this.txtWelcome, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtBooth, 2, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnLogin, 2, 8);
+            this.tableLayoutPanel1.Controls.Add(this.lblBooth, 1, 6);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 9;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.311201F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.11576F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.5231F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.648304F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.76906F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.77281F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.291599F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.25654F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.311632F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(454, 365);
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.618421F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.388158F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.30263F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.792731F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.91638F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.91526F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.91526F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.791055F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.09093F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.045752F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(649, 608);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // txtUser
@@ -78,35 +81,30 @@ namespace ProyectoPrueba
             this.txtUser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.SetColumnSpan(this.txtUser, 2);
             this.txtUser.Font = new System.Drawing.Font("Calisto MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtUser.Location = new System.Drawing.Point(139, 176);
-            this.txtUser.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUser.Location = new System.Drawing.Point(198, 275);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(294, 29);
+            this.txtUser.Size = new System.Drawing.Size(420, 40);
             this.txtUser.TabIndex = 4;
-            this.txtUser.TextChanged += new System.EventHandler(this.txtUser_TextChanged);
             // 
             // txtPassword
             // 
             this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.SetColumnSpan(this.txtPassword, 2);
             this.txtPassword.Font = new System.Drawing.Font("Calisto MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtPassword.Location = new System.Drawing.Point(139, 222);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPassword.Location = new System.Drawing.Point(198, 353);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(294, 29);
+            this.txtPassword.Size = new System.Drawing.Size(420, 40);
             this.txtPassword.TabIndex = 5;
-            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
             // 
             // user
             // 
             this.user.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.user.AutoSize = true;
             this.user.Font = new System.Drawing.Font("Calisto MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.user.Location = new System.Drawing.Point(20, 180);
-            this.user.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.user.Location = new System.Drawing.Point(28, 279);
             this.user.Name = "user";
-            this.user.Size = new System.Drawing.Size(115, 22);
+            this.user.Size = new System.Drawing.Size(164, 32);
             this.user.TabIndex = 7;
             this.user.Text = "Usuario:";
             // 
@@ -115,39 +113,21 @@ namespace ProyectoPrueba
             this.password.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.password.AutoSize = true;
             this.password.Font = new System.Drawing.Font("Calisto MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.password.Location = new System.Drawing.Point(20, 226);
-            this.password.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.password.Location = new System.Drawing.Point(28, 357);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(115, 22);
+            this.password.Size = new System.Drawing.Size(164, 32);
             this.password.TabIndex = 8;
             this.password.Text = "Contraseña:";
             // 
-            // btnLogIn
-            // 
-            this.btnLogIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnLogIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(30)))), ((int)(((byte)(89)))));
-            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogIn.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnLogIn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnLogIn.Location = new System.Drawing.Point(139, 294);
-            this.btnLogIn.Margin = new System.Windows.Forms.Padding(2);
-            this.btnLogIn.Name = "btnLogIn";
-            this.btnLogIn.Size = new System.Drawing.Size(175, 47);
-            this.btnLogIn.TabIndex = 9;
-            this.btnLogIn.Text = "Iniciar Sesión";
-            this.btnLogIn.UseVisualStyleBackColor = false;
-            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::ProyectoFinal.Properties.Resources.Gestor1;
-            this.pictureBox1.Location = new System.Drawing.Point(139, 53);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(198, 76);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(175, 100);
+            this.pictureBox1.Size = new System.Drawing.Size(250, 160);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
@@ -157,25 +137,59 @@ namespace ProyectoPrueba
             this.txtWelcome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txtWelcome.AutoSize = true;
             this.txtWelcome.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtWelcome.Location = new System.Drawing.Point(139, 20);
-            this.txtWelcome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtWelcome.Location = new System.Drawing.Point(198, 29);
             this.txtWelcome.Name = "txtWelcome";
-            this.txtWelcome.Size = new System.Drawing.Size(175, 26);
+            this.txtWelcome.Size = new System.Drawing.Size(250, 37);
             this.txtWelcome.TabIndex = 11;
             this.txtWelcome.Text = "Bienvenido/a";
             this.txtWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Login
+            // txtBooth
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.txtBooth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.txtBooth, 2);
+            this.txtBooth.Font = new System.Drawing.Font("Calisto MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBooth.Location = new System.Drawing.Point(198, 431);
+            this.txtBooth.Name = "txtBooth";
+            this.txtBooth.Size = new System.Drawing.Size(420, 40);
+            this.txtBooth.TabIndex = 12;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(30)))), ((int)(((byte)(89)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLogin.Location = new System.Drawing.Point(198, 516);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(250, 60);
+            this.btnLogin.TabIndex = 9;
+            this.btnLogin.Text = "Iniciar Sesión";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblBooth
+            // 
+            this.lblBooth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBooth.AutoSize = true;
+            this.lblBooth.Font = new System.Drawing.Font("Calisto MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblBooth.Location = new System.Drawing.Point(28, 419);
+            this.lblBooth.Name = "lblBooth";
+            this.lblBooth.Size = new System.Drawing.Size(164, 64);
+            this.lblBooth.TabIndex = 13;
+            this.lblBooth.Text = "Número de cabina:";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(208)))), ((int)(((byte)(241)))));
-            this.ClientSize = new System.Drawing.Size(454, 365);
+            this.ClientSize = new System.Drawing.Size(649, 608);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "Login";
+            this.Name = "frmLogin";
             this.Text = "Inicio de sesión";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -191,9 +205,10 @@ namespace ProyectoPrueba
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label user;
         private System.Windows.Forms.Label password;
-        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label txtWelcome;
+        private System.Windows.Forms.TextBox txtBooth;
+        private System.Windows.Forms.Label lblBooth;
     }
 }
-
--- Programación orientada a objetos/ProyectoFinal/View/Login.cs
+++ Programación orientada a objetos/ProyectoFinal/View/Login.cs
@@ -1,4 +1,5 @@
 ﻿using ProyectoFinal;
+using ProyectoFinal.ContextUCA;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,9 +12,9 @@ using System.Windows.Forms;
 
 namespace ProyectoPrueba
 {
-    public partial class Login : Form
+    public partial class frmLogin : Form
     {
-        public Login()
+        public frmLogin()
         {
             InitializeComponent();
         }
@@ -27,15 +28,75 @@ namespace ProyectoPrueba
         {
 
         }
-
-        private void btnLogIn_Click(object sender, EventArgs e)
+        private void AddLogin()
         {
-            //Irán validaciones para verificar que existe el usuario, que no pueda iniciar sesión sin haber ingresado algo
-            DialogResult answer = MessageBox.Show("Fecha, hora y id de cabina han sido guardados", "Inicio de sesión", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            this.Hide();
-            using (frmPrincipal newWindow = new frmPrincipal())
+            var db = new UCA_ContextContext();
+            var query = (from Employee in db.Employees
+                         from Administrator in db.Administrators
+                         where Administrator.Id == Employee.IdAdministrator
+                         && Employee.IdBooth == Int32.Parse(txtBooth.Text)
+                         orderby Administrator.NameUser
+                         select Employee.Id).Last();
+            string idEmployee = query;
+            DateTime dateTime = DateTime.Now;
+            var login = new Login
             {
-                newWindow.ShowDialog();
+                DateTime = dateTime,
+                IdEmployee = query,
+                IdBooth = Int32.Parse(txtBooth.Text)
+            };
+
+            db.Add(login);
+            db.SaveChanges();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var db = new UCA_ContextContext();
+            List<Administrator> administrators = db.Administrators
+                .ToList();
+            string user = txtUser.Text;
+            string pass = txtPassword.Text;
+            List<Administrator> result1 = administrators
+                .Where(a => a.NameUser == user &&
+                            a.PasswordUser == pass)
+                .ToList();
+
+            var query1 = (from Employee in db.Employees
+                          from Administrator in db.Administrators
+                          from Booth in db.Booths
+                          where Administrator.Id == Employee.IdAdministrator &&
+                          Booth.Id == Employee.IdBooth && Administrator.NameUser == txtUser.Text
+                          orderby Booth.Id
+                          select Booth.Id).Last();
+            int id = query1;
+
+            List<Booth> booths = db.Booths
+                .ToList();
+            int number = Int32.Parse(txtBooth.Text);
+            List<Booth> result2 = booths
+                .Where(a => id == number)
+                .ToList();
+
+            if (result1.Count() > 0 && result2.Count() > 0)
+            {
+                DateTime localDate = DateTime.Now;
+                DialogResult dataEmployee = MessageBox.Show("Fecha y hora de inicio de sesión: "
+                + localDate + " \nCabina en que sea iniciado sesión: " + number,
+                "Inicio de sesión", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                AddLogin();
+                this.Hide();
+                using (frmPrincipal window = new frmPrincipal())
+                {
+                    window.ShowDialog();
+                }
+            }
+            //Mensaje de error
+            else
+            {
+                MessageBox.Show("El Usuario y contraseña son incorrectos o el numero de cabina no existe!",
+                "Datos incorrectos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Irán validaciones para verificar que existe el usuario, que no pueda iniciar sesión sin haber ingresado algo
             }
         }
     }
